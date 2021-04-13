4336146b2c84620585ee73fa8236ceb9661b0d87
Jerwin Delos Santos <delossantosjerwin20@gmail.com>
2021-04-13 14:05:42 +0800

    Step 1 Done

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+//variables
+
+
+
+//Event Listeners
+eventListeners();
+function eventListeners(){
+    //form submission
+    document.querySelector('#form').addEventListener('submit', newTweet);
+}
+
+
+
+//Functions
+
+function newTweet(e){
+    e.preventDefault();
+
+    //Read Textarea value
+    const tweet = document.getElementById()
+}
